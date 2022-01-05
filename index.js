b4dc43cfb8dea450bbc00f53cc886c286308edd7
Bento Leandro <bentolrosolia@gmail.com>
2022-01-04 23:29:38 -0300

    Added welcome message

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 require('dotenv').config()
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
+
+client.on("ready", () => console.log(`Logged in as ${client.user.tag}`));
+
 client.on("messageCreate", (message) => {
     if (message.content === "hi"){
-        message.reply('Suuuuuup')
+        message.reply('Suuuuuup');
     }
 })
+const welcomeChannelId = 928110428733726791;
 
-client.on("ready", () => console.log(`Logged in as ${client.user.tag}`))
-
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to the server!`)
+})
 
 client.login(process.env.TOKEN)
